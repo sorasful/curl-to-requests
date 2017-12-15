c59c93b9de331027ca366e799a8a699be2710049
sorasful <teva.krief@hotmail.fr>
2017-12-15 12:58:15 +0100

    Beautify a little readme and code and create requirements.txt

--- README.md
+++ README.md
@@ -1,11 +1,37 @@
-# curl-to-requests-
-A CLI tool which converts a curl command to a Python requests command.
+# curl-to-requests
+A simple CLI tool which converts a curl command to a Python requests command.
 
-### Still in development !
+## Features 
 
-Usage `curl_to_requests.py curl 'http://example.com' -d {'user':}`
+Requires Python3.5+
 
-which will output a string corresponding to a the Python code for the requests. Just import requests and copy/paste it.  
+Supported features :
+- all HTTP verbs
+- headers
+- datas
+
+What's to come next ?  
+I'll add features when I'll encounter more uses case working with curl. 
+Stay tuned !
+
+## Install : 
+
+Clone the repository :  
+`git clone git@github.com:sorasful/curl-to-requests-.git`  
+then cd in the repository
+
+Create (or not ) a virtualenv and then install dependencies.  
+`pip install -r requirements.txt`
+
+## Usage:
+
+Usage:  
+`./curl_to_requests.py """curl 'http://example.com/' -H 'Accept-Encoding: gzip, deflate'  --compressed"""`
+
+which will output a string corresponding to a the Python code for the requests. Just import requests and copy/paste it. 
+
+If you want to see some informations concerning the application just run :  
+`./curl_to_requests --help`
 
 
-Enjoy 
+Enjoy !
--- curl_to_requests.py
+++ curl_to_requests.py
@@ -1,4 +1,4 @@
-import requests
+#!/usr/bin/env python
 import click
 import re
 
@@ -8,13 +8,19 @@ REGEX_HEADERS = r"""(-H ?(?:'|")[\w\s\\\/\.,;:\-\+=()\*]+)"""
 REGEX_DATAS = r"""(-d|--data)( ?'[\w\s{":\.,}]+')"""
 REGEX_VERB = r"""-X [\w]+"""
 
-# @click.command()
-# @click.argument('command')
+@click.command()
+@click.argument('command')
 def convert_curl_to_requests(command):
     """
-    Take a curl as parameter and return the str of the python to implement.
-    :param command:
-    :return:
+    Take a curl command enclosed in triple quotes as parameter and return the str of the python to implement directly copy and paste.
+    IMPORTANT: Enclose your curl command in triple quotes just like in the example provided.
+    Do not forget to import requests module when you try the result.
+
+    Example :
+    python3 curl_to_requests.py \"""curl 'http://example.com/' -H 'Accept-Encoding: gzip, deflate'  --compressed\"""
+
+    will return :
+    requests.get(url='http://example.com/',   headers={'Accept-\n  Encoding': 'gzip, deflate'}, )
     """
 
     address = ""
@@ -49,9 +55,4 @@ def convert_curl_to_requests(command):
 
 
 if __name__ == "__main__":
-
-    # TEST_COMMAND = "curl 'https://stackoverflow.com/questions/28568900/django-pagination-object-of-type-nonetype-has-no-len' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'referer: https://www.google.fr/' -H 'authority: stackoverflow.com' -H 'cookie: prov=50b62f78-637f-c6ff-3ad1-a64d03e675db; __qca=P0-1804862972-1508318898660; cc=bce0d12d6dbe4386a29c85993987db2e; _ga=GA1.2.472993284.1508318899; _gid=GA1.2.115529633.1513108052' --compressed"
-    # TEST_COMMAND = """curl -d '{"key1":"value1", "key2":"value2"}' -H "Content-Type: application/json" -X POST http://localhost:3000/data """
-    # TEST_COMMAND = """curl 'https://github.com/RRMoelker/django-markdownify' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.9' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Referer: https://www.google.fr/' -H 'Cookie: _octo=GH1.1.1777018281.1508277971; logged_in=yes; dotcom_user=sorasful; _ga=GA1.2.1964303453.1508277971; tz=Europe%2FParis; user_session=ZsdmRQ9PsYDZ6rUrl-w4TVpBwvQODg1teb8enr6PLDnLhYSP; __Host-user_session_same_site=ZsdmRQ9PsYDZ6rUrl-w4TVpBwvQODg1teb8enr6PLDnLhYSP; _gh_sess=eyJzZXNzaW9uX2lkIjoiYzU3ZDcxOWJkOTZlYzZjNjRkNzRmMmI2NTQ2NjVkNmEiLCJsYXN0X3JlYWRfZnJvbV9yZXBsaWNhcyI6MTUxMzMyODgwNTY1NiwiY29udGV4dCI6Ii8iLCJsYXN0X3dyaXRlIjoxNTEzMjc5NTAwODg5LCJzcHlfcmVwbyI6InNvcmFzZnVsL2N1cmwtdG8tcmVxdWVzdHMtIiwic3B5X3JlcG9fYXQiOjE1MTMzMjg4MDV9--f5fd8fab08606e7779fc74a007e1e95acd143caf' -H 'Connection: keep-alive' -H 'Cache-Control: max-age=0' --compressed"""
-    TEST_COMMAND = """curl 'http://docs.python-requests.org/en/master/user/quickstart/'  --compressed"""
-    convert_curl_to_requests(TEST_COMMAND)
+    convert_curl_to_requests()
